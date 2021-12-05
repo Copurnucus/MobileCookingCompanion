8f131febe0503406aeb439b8a4079d62308a0091
copurnucus <vbscopurnucus@gmail.com>
2021-12-05 01:08:07 -0500

    Step 2, Electric Boogaloo

--- app/src/main/java/com/example/mobilecookingcompanion/MainActivity.java
+++ app/src/main/java/com/example/mobilecookingcompanion/MainActivity.java
@@ -1,16 +1,7 @@
 package com.example.mobilecookingcompanion;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.MenuItem;
-import android.widget.AdapterView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -18,41 +9,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        BottomNavigationView bottomNav =findViewById(R.id.bottom_navigation);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,  new HomeFragment()).commit();
     }
-
-    public void setOnItemSelectedListener(@Nullable AdapterView.OnItemSelectedListener listener) {
-        AdapterView.OnItemSelectedListener selectedListener = listener;
-
-
-    }
-
-    // Test test -- GitHub Functionality Testing Here
-    // Can you see this?
-
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-
-            switch(item.getItemId()) {
-                case R.id.nav_home:
-                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.nav_pantry:
-                    selectedFragment = new PantryFragment();
-                    break;
-                case R.id.nav_recipes:
-                    selectedFragment = new RecipesFragment();
-                    break;
-                case R.id.nav_settings:
-                    selectedFragment = new SettingsFragment();
-                    break;
-            }
-
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-
-            return true;
-        }
-    }
-
+}
--- app/src/main/java/com/example/mobilecookingcompanion/PantryFragment.java
+++ app/src/main/java/com/example/mobilecookingcompanion/PantryFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.cookingapp.R;
+
 public class PantryFragment extends Fragment {
     @Nullable
     @Override
